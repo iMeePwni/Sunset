35bcf9e0e85555fd8994ee3ad62fed5ce9f0c528
guofeng <imeepwni@outlook.com>
2017-07-27 16:06:19 +0800

    将属性动画从代码中移动到xml中

--- app/src/main/java/com/imeepwni/android/sunset/ui/SunsetFragment.kt
+++ app/src/main/java/com/imeepwni/android/sunset/ui/SunsetFragment.kt
@@ -19,7 +19,7 @@ class SunsetFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentSunsetBinding>(inflater, R.layout.fragment_sunset, container, false)
         val view = binding.root
         view.setOnClickListener {
-            if (isDay) sunsetAnimation()
+            if (isDay) startSunsetAnimator()
             else sunupAnimation()
         }
         return view
@@ -27,55 +27,62 @@ class SunsetFragment : Fragment() {
 
     val start by lazy { sun.top.toFloat() }
     val end by lazy { sky.bottom.toFloat() }
-    val blueSky by lazy { getColor(R.color.blue_sky) }
-    val sunsetSky by lazy { getColor(R.color.sunset_sky) }
-    val nightSky by lazy { getColor(R.color.night_sky) }
 
-    private fun sunsetAnimation() {
-        isDay = false
-        val sunsetAnimator = ObjectAnimator.ofFloat(sun, "y", start, end).apply {
+    val sunsetAnimator by lazy {
+        ObjectAnimator.ofFloat(sun, "y", start, end).apply {
             duration = 3000
             interpolator = AccelerateInterpolator()
-        }
-
-        val sunsetSkyAnimator = ObjectAnimator.ofInt(sky, "backgroundColor", blueSky, sunsetSky).apply {
-            duration = 3000
+        }!!
+    }
+    val sunsetSkyAnimator by lazy {
+        getAnimator(R.animator.sunset_sky).apply {
+            target = sky
             setEvaluator(ArgbEvaluator())
         }
-
-        val nightAnimator = ObjectAnimator.ofInt(sky, "backgroundColor", sunsetSky, nightSky).apply {
-            duration = 1500
+    }
+    val nightAnimator by lazy {
+        getAnimator(R.animator.night_sky).apply {
+            target = sky
             setEvaluator(ArgbEvaluator())
         }
+    }
 
+    private fun getAnimator(resId: Int) = AnimatorInflater.loadAnimator(activity, resId) as ObjectAnimator
+
+    private fun startSunsetAnimator() {
+        isDay = false
         val set = AnimatorSet()
         set.play(sunsetAnimator).with(sunsetSkyAnimator).before(nightAnimator)
         set.start()
     }
 
-    private fun sunupAnimation() {
-        isDay = true
-        val dayAnimator = ObjectAnimator.ofInt(sky, "backgroundColor", nightSky, sunsetSky).apply {
-            duration = 1500
+    val dayAnimator by lazy {
+        getAnimator(R.animator.day_sky).apply {
+            target = sky
             setEvaluator(ArgbEvaluator())
         }
+    }
 
-        val sunupSkyAnimator = ObjectAnimator.ofInt(sky, "backgroundColor", sunsetSky, blueSky).apply {
-            duration = 3000
+    val sunupSkyAnimator by lazy {
+        getAnimator(R.animator.sunup_sky).apply {
+            target = sky
             setEvaluator(ArgbEvaluator())
         }
+    }
 
-        val sunupAnimator = ObjectAnimator.ofFloat(sun, "y", end, start).apply {
+    val sunupAnimator by lazy {
+        ObjectAnimator.ofFloat(sun, "y", end, start).apply {
             duration = 3000
             interpolator = AccelerateInterpolator()
-        }
+        }!!
+    }
 
+    private fun sunupAnimation() {
+        isDay = true
         val set = AnimatorSet()
         set.play(sunupAnimator).with(sunupSkyAnimator).after(dayAnimator)
         set.start()
     }
-
-    private fun getColor(resId: Int) = activity.getColor(resId)
 
     companion object {
         fun newInstance(): SunsetFragment {
